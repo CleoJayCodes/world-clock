d7a2e334fce67cebac39d5dfc8fb8794e5ea1f57
CleoJayCodes <jarnaeblackett@gmail.com>
2024-03-27 15:49:31 +0000

    added 'select' functionality

--- javascript/index.js
+++ javascript/index.js
@@ -27,5 +27,24 @@ function updateTime() {
   milanDateElement.innerHTML = milanTime.format("MMMM Do YYYY");
   milanTimeElement.innerHTML = milanTime.format("H:mm:ss [<small>]A[</small>]");
 }
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let cityName = cityTimeZone.split("/")[1];
+  let citiesElement = document.querySelector(".cities");
+  citiesElement.innerHTML = `<div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date"> ${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time"> ${cityTime.format(
+          "H:mm:ss [<small>]A[</small>]"
+        )}</div>
+      </div>`;
+}
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
